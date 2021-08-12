199ab6dfb59e9cca843450a281dbd45322f5ba1f
Daniele Grandi <daniele.grandi@autodesk.com>
2021-08-11 17:18:17 -0700

    Fixes examples.sh script

--- examples.sh
+++ examples.sh
@@ -1,6 +1,6 @@
 # Run the algorithm
-./build/ManifoldPlus --input data/bathtub.obj --output results/bathtub_manifold.obj
-./build/ManifoldPlus --input data/bed.obj --output results/bed_manifold.obj
-./build/ManifoldPlus --input data/chair.off --output results/chair_manifold.obj
-./build/ManifoldPlus --input data/table.off --output results/table_manifold.obj
+./build/manifold --input data/bathtub.obj --output results/bathtub_manifold.obj
+./build/manifold --input data/bed.obj --output results/bed_manifold.obj
+./build/manifold --input data/chair.off --output results/chair_manifold.obj
+./build/manifold --input data/table.off --output results/table_manifold.obj
 echo 'Check results in results folder. If you use meshlab, turn on double face rendering since faces from both sides of zero-volume structure could collide together.'
